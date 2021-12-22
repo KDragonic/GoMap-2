61a880241d25c94f3aeed6ce4b04887927f4fe82
Денис Коропов <denkoropov@gmail.com>
2021-12-22 17:34:30 +0300

    master

--- Сервер/routes/api.php
+++ Сервер/routes/api.php
@@ -19,5 +19,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::post('/user/login', 'UserController@login')->name('login');
-Route::post('/user/register', 'UserController@register')->name('register');
+Route::post('/user/login', [UserController::class, 'login'])->name('login');
+Route::post('/user/register', [UserController::class, 'register'])->name('register');
